2f46930d05b253bbeb14d0cebeeea3b50474dc01
Jason <topgunlinjr@gmail.com>
2019-05-30 16:44:00 -0400

    Configured Postgres

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,12 +2,33 @@
 
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 api = Api(app)
 
-app.config.from_object('project.config.DevelopmentConfig') 
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings) 
+
+# instatiate the database
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	username = db.Column(db.String(128), nullable=False)
+	email = db.Column(db.String(128), nullable=False)
+	active = db.Column(db.Boolean(), default=True, nullable=False)
+
+	def __init__(self, username, email):
+		self.username = username
+		self.email = email
+
+
 
 class UsersPing(Resource):
 	def get(self):
@@ -18,4 +39,5 @@ class UsersPing(Resource):
 
 
 api.add_resource(UsersPing, '/users/ping')
+
 
