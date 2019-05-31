8004c1671d585c1a84dd29c6ef89400a2ee5af03
Jason <topgunlinjr@gmail.com>
2019-05-31 10:47:11 -0400

    Removed from personal Github

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,43 +1,33 @@
 # services/users/project/__init__.py
 
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
-
-api = Api(app)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings) 
-
-# instatiate the database
-db = SQLAlchemy(app)
-
-# model
-class User(db.Model):
-	__tablename__ = 'users'
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	username = db.Column(db.String(128), nullable=False)
-	email = db.Column(db.String(128), nullable=False)
-	active = db.Column(db.Boolean(), default=True, nullable=False)
-
-	def __init__(self, username, email):
-		self.username = username
-		self.email = email
+#instantiate the database
+db = SQLAlchemy()
 
 
+def create_app(script_info=None):
 
-class UsersPing(Resource):
-	def get(self):
-		return {
-			'status': 'success',
-			'message':'pong!'
-		}
+	#instantiate the app
+	app = Flask(__name__)
+
+	#set config
+	app_settings = os.getenv('APP_SETTINGS')
+	app.config.from_object(app_settings)
+
+	#initiate the database
+	db.init_app(app)
 
 
-api.add_resource(UsersPing, '/users/ping')
+	#register blueprints
+	from project.api.users import users_blueprint
+	app.register_blueprint(users_blueprint)
 
+	# shell context for flask cli
+	@app.shell_context_processor
+	def ctx():
+		return { 'app': app, 'db': db}
 
+	return app
