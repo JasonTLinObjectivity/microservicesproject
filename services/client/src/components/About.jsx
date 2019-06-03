722da7cfc4856dc7bb2b4f1a1b915b54048d5322
Jason <topgunlinjr@gmail.com>
2019-06-02 21:48:14 -0400

    Added routing

---
+++ services/client/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+	return(
+		<div>
+		<h1 className="title is-1">About</h1>
+		<hr/>
+		<p>About us</p>
+		</div>
+)
+};
+
+export default About;
